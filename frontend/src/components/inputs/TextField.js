8d16835356198a6a2b7a2442a6449831467e3f78
Vic-L <vljc17@gmail.com>
2018-08-15 19:55:45 +0200

    style: text field

--- frontend/src/components/inputs/TextField.js
+++ frontend/src/components/inputs/TextField.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 const TextField = ({label, placeholder, type, name, onChange, value, error, ...others}) => {
-  return (~
-    .input-group
-      .input-group-label
-        {label}
-      %input(
-        className="input-group-field"
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        type={type}
-        onChange={onChange}
-        {...others})
-      .input-group-error
-        {error}
-  ~)
+  return (
+    <React.Fragment>
+      <div className='input-container'>
+        <input
+          className={`input ${error ? 'with-error' : ''}`}
+          name={name}
+          value={value || ""}
+          type={type}
+          onChange={onChange}
+          {...others}/>
+        <label className={`placeholder ${value ? 'not-empty' : ''}`}>
+          {placeholder}
+        </label>
+        <label className={`label ${value ? 'not-empty' : ''}`}>
+          {label}
+        </label>
+        <div className={`error-container ${error ? 'not-empty' : ''}`}>
+          <label className='error'>{error}</label>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default TextField
