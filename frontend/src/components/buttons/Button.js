f0706cdf1bf1c4508b26b3b7054b2108e2c917bf
Vic-L <vljc17@gmail.com>
2018-08-26 22:27:58 +0300

    feat: wrap Button in a container div to control alignment

--- frontend/src/components/buttons/Button.js
+++ frontend/src/components/buttons/Button.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Button = ({className, onClick, text, ...others}) => {
   return (
-    <div
-      className={className}
-      onClick={onClick}
-      {...others}>
-      {text}
+    <div className='button-container'>
+      <div
+        className={className}
+        onClick={onClick}
+        {...others}>
+        {text}
+      </div>
     </div>
   )
 }
