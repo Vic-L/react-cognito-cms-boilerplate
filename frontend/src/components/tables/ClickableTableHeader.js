a7df1e2560ee9f980f821365d5e314aad971b20c
Vic-L <vljc17@gmail.com>
2018-08-26 21:31:52 +0300

    feat: center sort double arrow in clickable table header

--- frontend/src/components/tables/ClickableTableHeader.js
+++ frontend/src/components/tables/ClickableTableHeader.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 class ClickableTableHeader extends React.Component {
   render() {
-    return (~
-      %th.clickable-table-header(onClick={this.props.onClick})
+    return (
+      <th className='clickable-table-header'
+        onClick={this.props.onClick}>
         {this.props.title}
-    ~)
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f5/Sort_both_small.svg" className="clickable-header-sort-arrows"/>
+      </th>
+    )
   }
 }
 
