ce4c43ad307d236592f9d7aa06382b862dbd3498
Vic-L <vljc17@gmail.com>
2018-08-27 10:55:48 +0300

    refactor: use spread operator to insert props to input element in TextField

--- frontend/src/components/inputs/TextField.js
+++ frontend/src/components/inputs/TextField.js
@@ -3,38 +3,26 @@ import React from 'react'
 const TextField = ({
   label,
   placeholder,
-  type,
-  name,
-  onChange,
-  onFocus,
-  onBlur,
   value,
   error,
   ...others
 }) => {
   return (
-    <React.Fragment>
-      <div className='input-container'>
-        <input
-          className={`input ${error ? 'with-error' : ''}`}
-          name={name}
-          value={value || ""}
-          type={type}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onChange={onChange}
-          {...others}/>
-        <label className={`placeholder ${value ? 'not-empty' : ''}`}>
-          {placeholder}
-        </label>
-        <label className={`label ${value ? 'not-empty' : ''}`}>
-          {label}
-        </label>
-        <div className={`error-container ${error ? 'not-empty' : ''}`}>
-          <label className='error'>{error}</label>
-        </div>
+    <div className='input-container'>
+      <input
+        className={`input ${error ? 'with-error' : ''}`}
+        value={value || ""}
+        {...others}/>
+      <label className={`placeholder ${value ? 'not-empty' : ''}`}>
+        {placeholder}
+      </label>
+      <label className={`label ${value ? 'not-empty' : ''}`}>
+        {label}
+      </label>
+      <div className={`error-container ${error ? 'not-empty' : ''}`}>
+        <label className='error'>{error}</label>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
