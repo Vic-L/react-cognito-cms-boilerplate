f04c21dc32e8212cc0ea91557ffdf7c08fa9ff73
Vic-L <vljc17@gmail.com>
2018-08-31 02:35:32 +0300

    fix: change loading reducer design

--- frontend/src/reducers/LoadingReducer.js
+++ frontend/src/reducers/LoadingReducer.js
@@ -1,10 +1,17 @@
-export const LoadingReducer = (state = false, action) => {
-  switch (action.type) {
-    case 'LOADING_START':
-    return true
-    case 'LOADING_END':
-    return false
-    default:
-    return state
+// inspired from https://medium.com/stashaway-engineering/react-redux-tips-better-way-to-handle-loading-flags-in-your-reducers-afda42a804c6
+// require all action types to follow naming convention with trailing _REQUEST/SUCCESS/FAILURE
+const LoadingReducer = (state = false, action) => {
+  const { type } = action;
+  const matches = /(.*)_(REQUEST|SUCCESS|FAILURE)/.exec(type);
+  
+  // not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
+  if (!matches) return state
+
+  const [, requestName, requestState] = matches;
+  return {
+    ...state,
+    [requestName]: requestState === 'REQUEST',
   }
 }
+
+export default LoadingReducer
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
-import { LoadingReducer } from '_reducers/LoadingReducer'
+import LoadingReducer from '_reducers/LoadingReducer'
 import { AlertReducer } from '_reducers/AlertReducer'
 
 export default combineReducers({
-  isLoading: LoadingReducer,
+  loading: LoadingReducer,
   alert: AlertReducer,
   routing: routerReducer,
 })
