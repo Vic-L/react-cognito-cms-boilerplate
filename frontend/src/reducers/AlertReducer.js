944c2490c69ecdb131d21f124be84b7e19d8c62d
Vic-L <vljc17@gmail.com>
2018-08-31 02:36:03 +0300

    fix: handle login failure alert message

--- frontend/src/reducers/AlertReducer.js
+++ frontend/src/reducers/AlertReducer.js
@@ -2,6 +2,13 @@ export const AlertReducer = (state = null, action) => {
   switch (action.type) {
     case 'ALERT_SHOW':
     return action.alert
+    
+    case 'LOGIN_FAILURE':
+    return {
+      title: "Alert",
+      body: action.message
+    }
+
     case 'ALERT_DISMISS':
     return null
     default:
