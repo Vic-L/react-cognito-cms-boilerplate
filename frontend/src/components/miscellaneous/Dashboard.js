23f3d23e75040a8134e010960a444b7dd55518f8
Vic-L <vljc17@gmail.com>
2018-10-03 10:54:25 +0500

    feat: integrate graphql

--- frontend/src/components/miscellaneous/Dashboard.js
+++ frontend/src/components/miscellaneous/Dashboard.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import Loadable from 'react-loadable'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 const Table = Loadable({
   loader: () => import('_tables/Table'),
   loading: () => <div></div>,
 })
 
+const getPostsQuery = gql`
+  query getPosts {
+    allPosts(count: 100) {
+      id
+      title
+    }
+  }
+`
+
 class Dashboard extends React.Component {
   render() {
-    return (~
-      %div
-        %Table
-    ~)
+    return (
+      <div>
+        <h1>GRAPHQL Posts</h1>
+        {this.renderPosts()}
+      </div>
+    )
+  }
+
+  renderPosts() {
+    if (_.isNil(this.props.data.allPosts)) {
+      return null
+    }
+    return this.props.data.allPosts.map((post) => {
+      return (
+        <p>{post.title}</p>
+      )
+    })
   }
 }
 
-export default Dashboard
+export default graphql(getPostsQuery)(Dashboard)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,81 @@ import { Provider } from  'react-redux'
 import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'react-router-redux'
 
+// authentication
+import Auth from '@aws-amplify/auth'
+
+Auth.configure({
+  // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+  // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+  
+  // REQUIRED - Amazon Cognito Region
+  region: process.env.COGNITO_REGION,
+
+  // OPTIONAL - Amazon Cognito Federated Identity Pool Region 
+  // Required only if it's different from Amazon Cognito Region
+  // identityPoolRegion: 'XX-XXXX-X',
+
+  // OPTIONAL - Amazon Cognito User Pool ID
+  userPoolId: process.env.COGNITO_ADMIN_USER_POOL_ID,
+
+  // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  userPoolWebClientId: process.env.COGNITO_ADMIN_CLIENT_ID,
+
+  // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+  mandatorySignIn: false,
+
+  // // OPTIONAL - Configuration for cookie storage
+  // cookieStorage: {
+  // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+  //     domain: '.yourdomain.com',
+  // // OPTIONAL - Cookie path
+  //     path: '/',
+  // // OPTIONAL - Cookie expiration in days
+  //     expires: 1,
+  // // OPTIONAL - Cookie secure flag
+  //     secure: true
+  // },
+
+  // OPTIONAL - customized storage object
+  // storage: new MyStorage(),
+  
+  // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+  // authenticationFlowType: 'USER_PASSWORD_AUTH'
+})
+
+// apollo
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createNetworkStatusNotifier } from 'react-apollo-network-status'
+
+// for listening to `loading` value in each graphql query/mutation on global level
+const {
+  NetworkStatusNotifier,
+  link: networkStatusNotifierLink
+} = createNetworkStatusNotifier()
+
+const apolloClient = new ApolloClient({
+  link: networkStatusNotifierLink.concat(new HttpLink({ uri: 'https://fakerql.com/graphql' })),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all',
+    },
+  }
+})
+
+// redux related
 import rootReducers from '_reducers'
 
 import App from '_components/App'
+import Loader from '_components/Loader'
 
 import WebFont from 'webfontloader'
 
@@ -46,9 +118,16 @@ WebFont.load({
 })
 
 render((
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>
+  <ApolloProvider client={apolloClient}>
+    <Provider store={store}>
+      <Router history={history}>
+        <div>
+          <NetworkStatusNotifier render={({loading, error}) => (
+            <Loader loading={loading} error={error}/>
+          )}/>
+          <App />
+        </div>
+      </Router>
+    </Provider>
+  </ApolloProvider>
 ), document.getElementById("app"))
