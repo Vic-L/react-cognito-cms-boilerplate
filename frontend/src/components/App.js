942385a5963b15a1535865c5a5dbeb07f1a6b4c1
Vic-L <vljc17@gmail.com>
2018-11-26 11:20:02 +0900

    fix: add leading / to decide transition component key to add to App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,7 @@ import Main from '_miscellaneous/Main'
 import Login from '_authentications/Login'
 
 const App = ({ dispatch, location }) => {
-  const transitionComponentKey = location.pathname === 'login' ? 'login' : 'main'
+  const transitionComponentKey = location.pathname === '/login' ? 'login' : 'main'
 
   return (
     <React.Fragment>
