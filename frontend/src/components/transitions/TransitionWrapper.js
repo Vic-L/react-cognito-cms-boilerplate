00ef3df559207b913ddef7bb03f83509e23d75ce
Vic-L <vljc17@gmail.com>
2018-11-26 10:16:31 +0900

    fix: use styled component for TransitionWrapper

--- frontend/src/components/transitions/TransitionWrapper.js
+++ frontend/src/components/transitions/TransitionWrapper.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`
+
 function TransitionWrapper(Component) {
   return class extends React.Component {
     render() {
       return (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-          }}>
-          
+        <Wrapper>
           <Component {...this.props}/>
-
-        </div>
+        </Wrapper>
       );
     }
   }
