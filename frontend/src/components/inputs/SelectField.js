93a6bfbf09a7657c100d278cfa8dedb050e3d6c3
Vic-L <vljc17@gmail.com>
2019-01-21 08:38:58 +0900

    feat: replace SelectField with native select

--- frontend/src/components/inputs/SelectField.js
+++ frontend/src/components/inputs/SelectField.js
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 import React from 'react'
 import autobind from 'autobind-decorator'
-import Select from 'react-select'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import { InputField } from '_contentLoaders'
 import SelectStyle from '_inputs/SelectStyle'
@@ -10,40 +10,42 @@ import SelectStyle from '_inputs/SelectStyle'
 const TextField = React.lazy(() => import('_inputs/TextField'))
 const SelectContainer = React.lazy(() => import('_inputs/SelectContainer'))
 
+const SelectElement = styled.select`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  opacity: 0;
+  cursor: pointer;
+`
+
 class SelectField extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isDropdownShown: false,
-      selectedOption: null
-    }
-  }
-
   render() {
-    const { error } = this.props
+    const { error, placeholder } = this.props
 
     return (
       <React.Suspense fallback={<InputField/>}>
         <SelectContainer>
-          <div>
-            <React.Suspense fallback={<InputField/>}>
-              <TextField
-                name={this.props.name}
-                placeholder={this.props.placeholder}
-                type={this.props.type}
-                label={this.props.label}
-                error={this.props.error}
-                value={this.renderOptionLabel()}
-                fieldIconSrc='https://upload.wikimedia.org/wikipedia/commons/4/4b/Feather-arrows-chevron-down.svg'
-                onChange={this.props.onChange}/>
-            </React.Suspense>
-          </div>
-          <Select
-            styles={SelectStyle}
-            value={this.renderOptionValue()}
+          <React.Suspense fallback={<InputField/>}>
+            <TextField
+              name={this.props.name}
+              placeholder={placeholder}
+              type={this.props.type}
+              label={this.props.label}
+              error={this.props.error}
+              value={this.renderOptionLabel()}
+              fieldIconSrc='https://upload.wikimedia.org/wikipedia/commons/4/4b/Feather-arrows-chevron-down.svg'
+              onChange={this.props.onChange}/>
+          </React.Suspense>
+          <SelectElement
             onChange={this.onSelectOption}
-            options={this.props.options}/>
+            value={this.renderOptionValue()}>
+            <option value='' disabled>{placeholder}</option>
+            {this.renderOptions()}
+          </SelectElement>
+          
         </SelectContainer>
       </React.Suspense>
     )
@@ -53,7 +55,7 @@ class SelectField extends React.Component {
   renderOptionLabel() {
     if (!_.isNil(this.props.value)) {
       const option = _.find(this.props.options, (option) => {
-        return option.value === this.props.value
+        return `${option.value}` === `${this.props.value}`
       })
 
       if (_.isNil(option)) {
@@ -69,17 +71,30 @@ class SelectField extends React.Component {
   @autobind
   renderOptionValue() {
     if (!_.isNil(this.props.value)) {
-      return _.find(this.props.options, (option) => {
-        return option.value === this.props.value
-      }) || null
+      const option = _.find(this.props.options, (option) => {
+        return `${option.value}` === `${this.props.value}`
+      })
+
+      if (_.isNil(option)) {
+        return ''
+      } else {
+        return option.value
+      }
     } else {
-      return null
+      return ''
     }
   }
 
   @autobind
-  onSelectOption(selectedOption) {
-    this.props.onChange(selectedOption.value)
+  renderOptions() {
+    return this.props.options.map(option => {
+      return <option key={option.label} value={option.value}>{option.label}</option>
+    })
+  }
+
+  @autobind
+  onSelectOption(e) {
+    this.props.onChange(e.target.value)
   }
 }
 
@@ -93,7 +108,15 @@ SelectField.propTypes = {
     PropTypes.number
   ]),
   error: PropTypes.string,
-  options: PropTypes.array.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ])
+    })
+  ).isRequired,
   onChange: PropTypes.func.isRequired,
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
