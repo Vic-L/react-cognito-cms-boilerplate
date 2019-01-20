4688baf7cc133a613678971b61f40aadba3cbcea
Vic-L <vljc17@gmail.com>
2019-01-20 12:01:01 +0900

    cleanup: remove sagas

--- frontend/src/sagas/alerts.js
+++
@@ -1,9 +0,0 @@
-import { call, takeLatest } from 'redux-saga/effects'
-
-export function* alertsSaga() {
-  yield takeLatest('ALERT_SHOW', showAlert)
-}
-
-export function* showAlert({ alert }) {
-  yield({type: 'ALERT_SHOW', alert})
-}
