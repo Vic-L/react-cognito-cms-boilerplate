1b0e11c7b1bea5ea9fe6d2166ed91656eb425830
Vic-L <vljc17@gmail.com>
2019-02-13 10:10:57 +0900

    feat: implement examples of victory

--- frontend/src/components/charts/Charts.js
+++ frontend/src/components/charts/Charts.js
@@ -1,140 +1,26 @@
 import React from 'react';
-import { Line, Pie } from 'react-chartjs-2';
-import { Grid, Cell } from 'styled-css-grid';
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryBar,
+  VictoryPie,
+} from 'victory';
+import {
+  Grid,
+  Cell,
+} from 'styled-css-grid';
 
 import CardContainer from '_cards/CardContainer';
 
 class Charts extends React.Component {
-  getLineChartData() {
-    return {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          fillColor: 'rgba(220,220,220,0.2)',
-          strokeColor: 'rgba(220,220,220,1)',
-          pointColor: 'rgba(220,220,220,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: 'My Second dataset',
-          fillColor: 'rgba(151,187,205,0.2)',
-          strokeColor: 'rgba(151,187,205,1)',
-          pointColor: 'rgba(151,187,205,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(151,187,205,1)',
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    };
+  getData() {
+    return [
+      {quarter: 1, earnings: 13000},
+      {quarter: 2, earnings: 16500},
+      {quarter: 3, earnings: 14250},
+      {quarter: 4, earnings: 19000}
+    ];
   }
-
-  getLineChartOptions() {
-    return {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-
-      //String - A legend template
-      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
-
-      //Boolean - Whether to horizontally center the label and point dot inside the grid
-      offsetGridLines : false,
-
-      responsive: true
-    }
-  }
-
-  getPieChartData() {
-    return {
-      datasets: [{
-        data: [10, 20, 30]
-      }],
-
-      // These labels appear in the legend and in the tooltips when hovering different arcs
-      labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-      ]
-    }
-  }
-
-  getPieChartOptions() {
-    return {
-      //Boolean - Whether we should show a stroke on each segment
-      segmentShowStroke : true,
-
-      //String - The colour of each segment stroke
-      segmentStrokeColor : "#fff",
-
-      //Number - The width of each segment stroke
-      segmentStrokeWidth : 2,
-
-      //Number - The percentage of the chart that we cut out of the middle
-      percentageInnerCutout : 0, // This is 50 for Doughnuts charts
-
-      //Number - Amount of animation steps
-      animationSteps : 100,
-
-      //String - Animation easing effect
-      animationEasing : "easeOutBounce",
-
-      //Boolean - Whether we animate the rotation of the Doughnut
-      animateRotate : true,
-
-      //Boolean - Whether we animate scaling the Doughnut from the centre
-      animateScale : false,
-
-      //String - A legend template
-      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>",
-
-      responsive: true
-    }
-  }
-
   render() {
     return (
       <React.Fragment>
@@ -145,10 +31,23 @@ class Charts extends React.Component {
             <React.Suspense fallback={<div />}>
               <CardContainer>
                 {() =>
-                  <Line
-                    data={this.getLineChartData()}
-                    options={this.getLineChartOptions()}
-                  />
+                  <VictoryChart
+                    domainPadding={20}
+                  >
+                    <VictoryAxis
+                      tickValues={[1, 2, 3, 4]}
+                      tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}
+                    />
+                    <VictoryAxis
+                      dependentAxis
+                      tickFormat={(x) => (`$${x / 1000}k`)}
+                    />
+                    <VictoryBar
+                      data={this.getData()}
+                      x="quarter"
+                      y="earnings"
+                    />
+                  </VictoryChart>
                 }
               </CardContainer>
             </React.Suspense>
@@ -157,9 +56,12 @@ class Charts extends React.Component {
             <React.Suspense fallback={<div />}>
               <CardContainer>
                 {() =>
-                  <Pie
-                    data={this.getPieChartData()}
-                    options={this.getPieChartOptions()}
+                  <VictoryPie
+                    data={[
+                      { x: 'Cats', y: 35 },
+                      { x: 'Dogs', y: 40 },
+                      { x: 'Birds', y: 55 }
+                    ]}
                   />
                 }
               </CardContainer>
